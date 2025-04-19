14b0b060977fa843bdc88561067773f9430ced3e
Nina Hammer <nina.hammer@mni.thm.de>
2025-04-19 14:27:10 +0200

    refactor block graph

--- src/base_blocks/mod.rs
+++ src/base_blocks/mod.rs
@@ -1,24 +1,25 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 use crate::code_gen::{Quadrupel, QuadrupelArg, QuadrupelOp, QuadrupelResult};
 
+type BlockId = usize;
 #[derive(Debug)]
 pub struct Block {
-    lable: Option<String>,
+    label: Option<String>,
     content: BlockContent,
 }
 impl Block {
-    fn new(lable: Option<String>, content: BlockContent) -> Self {
-        Block { lable, content }
+    fn new(label: Option<String>, content: BlockContent) -> Self {
+        Block { label, content }
     }
-    fn new_start(lable: Option<String>) -> Self {
-        Self::new(lable, BlockContent::Start)
+    fn new_start(label: Option<String>) -> Self {
+        Self::new(label, BlockContent::Start)
     }
-    fn new_stop(lable: Option<String>) -> Self {
-        Self::new(lable, BlockContent::Stop)
+    fn new_stop(label: Option<String>) -> Self {
+        Self::new(label, BlockContent::Stop)
     }
-    fn new_code(lable: Option<String>) -> Self {
-        Self::new(lable, BlockContent::Code(Vec::new()))
+    fn new_code(label: Option<String>) -> Self {
+        Self::new(label, BlockContent::Code(Vec::new()))
     }
 
     fn get_code_mut(&mut self) -> &mut Vec<Quadrupel> {
@@ -38,31 +39,30 @@ pub enum BlockContent {
 
 #[derive(Default)]
 pub struct BlockGraph {
-    blocks: HashMap<usize, Block>,
-    next_block_id: usize,
-    edges: HashMap<usize, Vec<usize>>,
+    blocks: Vec<Block>,
+    edges: Vec<HashSet<BlockId>>,
+    _label_to_id: HashMap<String, BlockId>,
 }
 
 impl BlockGraph {
     pub fn from_tac(code: &[Quadrupel]) -> Self {
-        let mut code = code.iter().cloned();
+        let code = code.iter().cloned();
         let mut graph = BlockGraph::default();
 
         let block_start = Block::new_start(None);
-        let (mut last_block_id, block_start) = graph.add_block(block_start, None);
+        let (last_block_id, block_start) = graph.add_block(block_start, None);
 
-        let mut block_active = Block::new_code(None);
-        let (mut block_active_id, block_active) =
-            graph.add_block(block_active, Some(last_block_id));
+        let block_active = Block::new_code(None);
+        let (block_active_id, block_active) = graph.add_block(block_active, Some(last_block_id));
         let block_active_code = block_active.get_code_mut();
 
-        while let Some(quad) = code.next() {
+        for quad in code {
             match quad {
                 Quadrupel {
                     op: QuadrupelOp::Default,
                     arg1: QuadrupelArg::Empty,
                     arg2: QuadrupelArg::Empty,
-                    result: QuadrupelResult::Label(label),
+                    result: QuadrupelResult::Label(_label),
                 } => {
                     // TODO: new block
                 }
@@ -77,19 +77,17 @@ impl BlockGraph {
     }
 
     fn add_block(&mut self, block: Block, parent: Option<usize>) -> (usize, &mut Block) {
-        let id = self.next_block_id;
-        self.next_block_id += 1;
-
+        self.blocks.push(block);
         if let Some(parent) = parent {
-            self.add_edge(parent, id);
+            self.add_edge(parent, self.blocks.len() - 1);
         }
 
-        let block = self.blocks.entry(id).insert_entry(block).into_mut();
+        let id = self.blocks.len() - 1;
 
-        (id, block)
+        (id, &mut self.blocks[id])
     }
 
     fn add_edge(&mut self, start: usize, end: usize) {
-        self.edges.entry(start).or_default().push(end);
+        self.edges[start].insert(end);
     }
 }
