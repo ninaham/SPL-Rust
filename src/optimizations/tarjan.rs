6591ed255c64005d196a2149cfafbbba007303c3
Florian Mueller <florian.mueller-4@mni.thm.de>
2025-06-17 15:09:55 +0200

    some clippy magic

--- src/optimizations/tarjan.rs
+++ src/optimizations/tarjan.rs
@@ -1,28 +1,17 @@
+#![allow(clippy::too_many_arguments)]
 use std::collections::{HashMap, HashSet};
 
 use crate::base_blocks::{Block, BlockGraph};
-
-type BlockId = usize;
 
 #[derive(Debug, Default)]
 pub struct Scc<'a> {
     pub scc: Vec<Vec<&'a Block>>,
 }
 
-//TODO: Umbenennen Bob, Datenstruktur für einzelne SCC: Liste Blöcke und Parent (optional), strong_connect Argumente so, dass Clippy nicht mehr nervt
-
-struct Bob<'a> {
-    id: BlockId,
-    index: &mut usize,
-    index_map: &mut HashMap<BlockId, usize>,
-    lowlink_map: &mut HashMap<BlockId, usize>,
-    on_stack: &mut HashSet<BlockId>,
-    stack: &mut Vec<BlockId>,
-    sccs: &mut Vec<Vec<&'a Block>>,
-}
+type BlockId = usize;
 
 impl BlockGraph {
-    pub fn tarjan(&self) -> Scc {
+    pub fn tarjan(&self) -> Scc<'_> {
         let mut index = 0;
         let mut index_map = HashMap::<BlockId, usize>::new();
         let mut lowlink_map = HashMap::<BlockId, usize>::new();
@@ -32,55 +21,67 @@ impl BlockGraph {
 
         for id in 0..self.blocks.len() {
             if !index_map.contains_key(&id) {
-                let mut bob = Bob {
-                    id: &id,
-                    index: &mut index,
-                    index_map: &mut index_map,
-                    lowlink_map: &mut lowlink_map,
-                    on_stack: &mut on_stack,
-                    stack: &mut stack,
-                    sccs: &mut sccs,
-                };
-                self.strong_connect(bob);
+                self.strong_connect(
+                    id,
+                    &mut index,
+                    &mut index_map,
+                    &mut lowlink_map,
+                    &mut on_stack,
+                    &mut stack,
+                    &mut sccs,
+                );
             }
         }
 
         Scc { scc: sccs }
     }
 
-    fn strong_connect<'a>(&'a self, bob: Bob) {
-        bob.index_map.insert(bob.id, bob.index);
-        bob.lowlink_map.insert(bob.id, *bob.index);
-        *bob.index += 1;
+    fn strong_connect<'a>(
+        &'a self,
+        id: BlockId,
+        index: &mut usize,
+        index_map: &mut HashMap<BlockId, usize>,
+        lowlink_map: &mut HashMap<BlockId, usize>,
+        on_stack: &mut HashSet<BlockId>,
+        stack: &mut Vec<BlockId>,
+        sccs: &mut Vec<Vec<&'a Block>>,
+    ) {
+        index_map.insert(id, *index);
+        lowlink_map.insert(id, *index);
+        *index += 1;
 
-        bob.stack.push(bob.id);
-        bob.on_stack.insert(bob.id);
+        stack.push(id);
+        on_stack.insert(id);
 
-        for next_block in &self.edges[bob.id] {
-            if !bob.index_map.contains_key(next_block) {
-                let mut bob = Bob {
-                    id: *next_block,
-                    ..bob
-                };
-                self.strong_connect(bob);
-                let lowlink = bob.lowlink_map[&bob.id].min(bob.lowlink_map[next_block]);
-                bob.lowlink_map.insert(bob.id, lowlink);
-            } else if bob.on_stack.contains(next_block) {
-                let lowlink = bob.lowlink_map[&bob.id].min(bob.index_map[next_block]);
-                bob.lowlink_map.insert(bob.id, lowlink);
+        for next_block in &self.edges[id] {
+            if !index_map.contains_key(next_block) {
+                self.strong_connect(
+                    *next_block,
+                    index,
+                    index_map,
+                    lowlink_map,
+                    on_stack,
+                    stack,
+                    sccs,
+                );
+                let lowlink = lowlink_map[&id].min(lowlink_map[next_block]);
+                lowlink_map.insert(id, lowlink);
+            } else if on_stack.contains(next_block) {
+                let lowlink = lowlink_map[&id].min(index_map[next_block]);
+                lowlink_map.insert(id, lowlink);
             }
         }
 
-        if bob.lowlink_map[&bob.id] == bob.index_map[&bob.id] {
+        if lowlink_map[&id] == index_map[&id] {
             let mut component = Vec::new();
-            while let Some(w) = bob.stack.pop() {
-                bob.on_stack.remove(&w);
+            while let Some(w) = stack.pop() {
+                on_stack.remove(&w);
                 component.push(&self.blocks[w]);
-                if w == bob.id {
+                if w == id {
                     break;
                 }
             }
-            bob.sccs.push(component);
+            sccs.push(component);
         }
     }
 }
