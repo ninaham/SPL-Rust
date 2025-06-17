ecede309804bd65f0c701dd5f06df933658f7b5b
Florian Mueller <florian.mueller-4@mni.thm.de>
2025-06-17 14:29:34 +0200

    now with bob

--- src/optimizations/tarjan.rs
+++ src/optimizations/tarjan.rs
@@ -2,12 +2,22 @@ use std::collections::{HashMap, HashSet};
 
 use crate::base_blocks::{Block, BlockGraph};
 
+type BlockId = usize;
+
 #[derive(Debug, Default)]
 pub struct Scc<'a> {
     pub scc: Vec<Vec<&'a Block>>,
 }
 
-type BlockId = usize;
+struct Bob<'a> {
+    id: BlockId,
+    index: &mut usize,
+    index_map: &mut HashMap<BlockId, usize>,
+    lowlink_map: &mut HashMap<BlockId, usize>,
+    on_stack: &mut HashSet<BlockId>,
+    stack: &mut Vec<BlockId>,
+    sccs: &mut Vec<Vec<&'a Block>>,
+}
 
 impl BlockGraph {
     pub fn tarjan(&self) -> Scc {
@@ -20,67 +30,55 @@ impl BlockGraph {
 
         for id in 0..self.blocks.len() {
             if !index_map.contains_key(&id) {
-                self.strong_connect(
-                    id,
-                    &mut index,
-                    &mut index_map,
-                    &mut lowlink_map,
-                    &mut on_stack,
-                    &mut stack,
-                    &mut sccs,
-                );
+                let mut bob = Bob {
+                    id: &id,
+                    index: &mut index,
+                    index_map: &mut index_map,
+                    lowlink_map: &mut lowlink_map,
+                    on_stack: &mut on_stack,
+                    stack: &mut stack,
+                    sccs: &mut sccs,
+                };
+                self.strong_connect(bob);
             }
         }
 
         Scc { scc: sccs }
     }
 
-    fn strong_connect<'a>(
-        &'a self,
-        id: BlockId,
-        index: &mut usize,
-        index_map: &mut HashMap<BlockId, usize>,
-        lowlink_map: &mut HashMap<BlockId, usize>,
-        on_stack: &mut HashSet<BlockId>,
-        stack: &mut Vec<BlockId>,
-        sccs: &mut Vec<Vec<&'a Block>>,
-    ) {
-        index_map.insert(id, *index);
-        lowlink_map.insert(id, *index);
-        *index += 1;
+    fn strong_connect<'a>(&'a self, bob: Bob) {
+        bob.index_map.insert(bob.id, bob.index);
+        bob.lowlink_map.insert(bob.id, *bob.index);
+        *bob.index += 1;
 
-        stack.push(id);
-        on_stack.insert(id);
+        bob.stack.push(bob.id);
+        bob.on_stack.insert(bob.id);
 
-        for next_block in &self.edges[id] {
-            if !index_map.contains_key(next_block) {
-                self.strong_connect(
-                    *next_block,
-                    index,
-                    index_map,
-                    lowlink_map,
-                    on_stack,
-                    stack,
-                    sccs,
-                );
-                let lowlink = lowlink_map[&id].min(lowlink_map[next_block]);
-                lowlink_map.insert(id, lowlink);
-            } else if on_stack.contains(next_block) {
-                let lowlink = lowlink_map[&id].min(index_map[next_block]);
-                lowlink_map.insert(id, lowlink);
+        for next_block in &self.edges[bob.id] {
+            if !bob.index_map.contains_key(next_block) {
+                let mut bob = Bob {
+                    id: *next_block,
+                    ..bob
+                };
+                self.strong_connect(bob);
+                let lowlink = bob.lowlink_map[&bob.id].min(bob.lowlink_map[next_block]);
+                bob.lowlink_map.insert(bob.id, lowlink);
+            } else if bob.on_stack.contains(next_block) {
+                let lowlink = bob.lowlink_map[&bob.id].min(bob.index_map[next_block]);
+                bob.lowlink_map.insert(bob.id, lowlink);
             }
         }
 
-        if lowlink_map[&id] == index_map[&id] {
+        if bob.lowlink_map[&bob.id] == bob.index_map[&bob.id] {
             let mut component = Vec::new();
-            while let Some(w) = stack.pop() {
-                on_stack.remove(&w);
+            while let Some(w) = bob.stack.pop() {
+                bob.on_stack.remove(&w);
                 component.push(&self.blocks[w]);
-                if w == id {
+                if w == bob.id {
                     break;
                 }
             }
-            sccs.push(component);
+            bob.sccs.push(component);
         }
     }
 }
