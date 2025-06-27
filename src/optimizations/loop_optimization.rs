1c5a087536ef1f416ef24ebd768b211d1b3fa635
Florian Mueller <florian.mueller-4@mni.thm.de>
2025-06-27 12:14:39 +0200

    switching branches

--- src/optimizations/loop_optimization.rs
+++ src/optimizations/loop_optimization.rs
@@ -1,5 +1,21 @@
-use crate::base_blocks::BlockGraph;
+use crate::{
+    base_blocks::BlockGraph,
+    optimizations::{
+        constant_propagation::ConstantPropagation,
+        live_variables::{self, LiveVariables},
+        tarjan::Scc,
+    },
+};
 
 impl BlockGraph {
-    pub fn loop_optimization(&mut self) {}
+    pub fn loop_optimization(
+        &mut self,
+        live_variables: &LiveVariables,
+        constant: &ConstantPropagation,
+    ) {
+        let sccs: Vec<Scc> = self.sccs.unwrap_or(|| vec![]);
+        sccs = sccs.sort_by_key(|scc| scc.nodes.len());
+
+        for scc in sccs {}
+    }
 }
