f0cc8d92e9809637c1bf684517666721a48099e1
Florian Mueller <florian.mueller-4@mni.thm.de>
2025-06-30 17:37:15 +0200

    bob

--- src/optimizations/loop_optimization.rs
+++ src/optimizations/loop_optimization.rs
@@ -1,17 +1,18 @@
-/*
-use crate::{
-    base_blocks::BlockGraph,
-    optimizations::{
-        constant_propagation::ConstantPropagation, live_variables::LiveVariables, tarjan::Scc,
-    },
-};
+use crate::{base_blocks::BlockGraph, optimizations::live_variables::LiveVariables};
 
 impl BlockGraph {
-    pub fn loop_optimization(
-        &mut self,
-        live_variables: &LiveVariables,
-        constant: &ConstantPropagation,
-    ) {
+    pub fn loop_optimization(&mut self, live_variables: &LiveVariables) {
+        if let Some(sccs_sorted) = self.sccs {
+            self.sccs
+                .as_mut()
+                .expect("Here should be something")
+                .sort_by_key(|scc| (scc.nodes.len()));
+
+            for scc in sccs_sorted.iter() {
+                for id in &scc.nodes {
+                    let mut block = 0;
+                }
+            }
+        };
     }
 }
-*/
